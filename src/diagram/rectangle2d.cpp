f5931f79f058970a4b6467693d45f292bdc7d53e
Jianye Xia <jyxia@jyxia.(none)>
2016-10-29 00:01:13 +0200

    delete inner subfolders

--- src/diagram/rectangle2d.cpp
+++
@@ -1,5 +0,0 @@
-#include "rectangle2d.h"
-
-Rectangle2D::Rectangle2D()
-{
-}
--- src/interface/test/test.cpp
+++
@@ -1,9 +0,0 @@
-#include "../treedata.h"
-int main()
-{
-    voronoi::TreeData treeData;
-    treeData.addLink("subProject1","Project1");
-    treeData.addLink("sim1","subProject1");
-    treeData.addLink("sim2","subProject1");
-    return 0;
-}
